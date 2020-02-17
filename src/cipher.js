53e8d35f7f171685a7719823807d45fe566ab998
ZayraJero <zayrajero@gmail.com>
2020-02-17 10:28:35 -0600

    read me

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,47 @@
-const cipher = {
-  // ...
+const cipher = { encode, decode};
+ 
+
+// se declara la funcion para codificar
+function encode (offset, string){
+let cipherTxt=""; //variable para dar el mensaje codificado
+
+  for (let i= 0; i < string.length; i++){ 
+    let i= string.charCodeAt(i);
+    let character = string[i];
+
+    if (string.charCodeAt(i) >= 65 && string.charCodeAt(i) <= 90){
+      let word = (string.charCodeAt(i) - 65 + parseInt(offset)) % 26 + 65;
+      cipherTxt += string.fromCharCode(word);
+      
+    } else if (string.charCodeAt(i) >= 97 && string.charCodeAt(i) <=122) {
+      word = (string.charCodeAt(i) -97 + parseInt(offset)) % 26 + 97;
+    cipherTxt += string.fromCharCode(word);
+    }
+} else {
+  cipherTxt += character;
+}
+
+return cipherTxt;
 };
 
-export default cipher;
+function decode: (string, offset) {
+  let dechiperTxt ="";
+
+  for(let i = 0; i < string.lenght; i++) {
+    let character1 = string[i];
+
+    if (string.charCodeAt(i) >= 65 && string. charCodeAt(i) <= 90) {
+      let word = (string.charCodeAt(i) + 65 - parseInt(offset)) % 26 + 65;dechiperTxt += string.fromCharCode(word);
+
+    } else if (string.charCodeAt(i) >= 97 && string.charCodeAt(i) <= 122){
+    let word = (string.fromCharCode(i) -97 - parseInt(offset) + 52) % 26) + 97;
+    dechiperTxt += string.fromCharCode(word);
+    }
+  }else {
+    dechiperTxt += character1;
+  }
+return dechiperTxt;
+
+};
+
+  export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,48 @@
 import cipher from './cipher.js';
 
 console.log(cipher);
+
+let word = document.getElementById("string");
+let offset = document.getElementById("offset");
+
+let submitButton = document.querySelector('#primerBoton')
+function myFunction () {
+  document.getElementById("primerBoton").addEventListener('click,myFunction');
+
+  document.getElementById("second-page").style.display="block";
+  document.getElementById("first-page").style.display="none";
+}
+
+submitButton.addEventListener('click,myFynction')
+
+let finalButton = document.querySelector('#segundoBoton')
+
+function myFunction2 () {
+  document.getElementById("segundoBoton").addEventListener('click,myFunction2');
+
+document.getElementById("third-page").style.display="block";
+document.getElementById("second-page").style.display="none";
+}
+
+submitButton.addEventListener('click,myFunction2')
+
+function encode(){
+
+let nombre = cipher.encode(offset.value, word.value);
+ document.getElementById("finaltext").innerText=nombre;
+third-page.style="display:block";
+}
+
+function decode(){
+
+let nombre = cipher.decode(offset.value, word.value);
+document.getElementById("finaltxt").innerText=nombre;
+third-page.style="display:block";
+}
+
+function back(){
+  document.location.reload(true);
+}
+
+document.getElementById("encode").addEventListener("click", encode);
+document.getElementById("decode").addEventListener("click",decode);
